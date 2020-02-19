386418f8cc9a49016087f1b5748f92efdec58450
Dishasingh16 <61135165+Dishasingh16@users.noreply.github.com>
2020-02-19 03:58:29 -0800

    Add files via upload
    collection part 3

---
+++ linkedlist.java
@@ -0,0 +1,43 @@
+package test;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+		
+		public class linkedlist {
+
+		public static void main(String[] args) {
+			LinkedList<String> list= new LinkedList<>();
+			list.add("hi");
+			list.add("hello");
+			list.add("bye");
+			list.add("7");
+			list.add("true");
+			ListIterator<String> it = list.listIterator();
+			System.out.println("****fwd only****");
+			while(it.hasNext()) {
+				String element =it.next();
+				System.out.println(element);
+				
+			}
+			System.out.println("****back only****");
+			while(it.hasPrevious()) {
+				String element =it.previous();
+				System.out.println(element);
+			}
+			
+			/*System.out.println(list);
+			list.remove(2);
+			list.add(2, "world");
+			for(String obj : list) {
+				System.out.println(obj);*/
+					
+				
+			}
+			
+			
+		}
+
+
