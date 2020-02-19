51a3c77e90a68470f504ea79ead0213eb7a73d72
Dishasingh16 <61135165+Dishasingh16@users.noreply.github.com>
2020-02-19 03:54:36 -0800

    Add files via upload

---
+++ antim.java
@@ -0,0 +1,29 @@
+package test;
+
+class X{
+	public  final int AGE;
+	X(){
+		AGE=10;
+	}
+	X(int x){
+		AGE=20;
+	}
+	
+}
+
+
+
+
+
+
+
+
+public class antim {
+
+public static void main(String args[]) {
+	
+		
+		
+	}
+}
+
