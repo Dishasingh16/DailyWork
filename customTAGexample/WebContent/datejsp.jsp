b4670af3ad3085b47f125f5e392ceffeecb7cf3a
Dishasingh16 <61135165+Dishasingh16@users.noreply.github.com>
2020-03-13 04:38:17 -0700

    custom tag

--- customTAGexample/WebContent/datejsp.jsp
+++ customTAGexample/WebContent/datejsp.jsp
@@ -1,13 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-    <%@taglib prefix="pd" uri="/WEB-INF/tlds/tagLibraryDescriptor.tld" %>
+	pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="pd" uri="/WEB-INF/tlds/tagLibraryDescriptor.tld"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
++ 
 </head>
 <body>
-<pd:printDate></pd:printDate>
+	<h1>from date.jsp page</h1>
+	<pd:printDate>
+		<h1>inside printDate tag body</h1>
+	</pd:printDate>
+	<h1>outside printDate tag body</h1>
+	<h2>end of page</h2>
 </body>
 </html>
--- customTAGexample/src/com/cts/customtag/TagHandler.java
+++ customTAGexample/src/com/cts/customtag/TagHandler.java
@@ -1,19 +1,35 @@
 package com.cts.customtag;
 
+import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class TagHandler extends TagSupport {
 	@Override
 	public int doStartTag() throws JspException {
 		System.out.println("dostarttag");
-		return super.doStartTag();
+		
+		JspWriter out=pageContext.getOut();
+		
+		try {
+			out.write("<h1>"+new Date()+"</h1>");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return EVAL_BODY_INCLUDE;
+		//return SKIP_BODY;
 	}
 
 	@Override
 	public int doEndTag() throws JspException {
 		System.out.println("doendtag");
-		return super.doEndTag();
+//return EVAL_PAGE;
+		return SKIP_PAGE;
 	}
 
 }
